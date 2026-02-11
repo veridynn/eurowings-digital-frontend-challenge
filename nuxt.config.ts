0d6ce769e87704830987f2be42522feead1b56f9
Bogdan <35661226+veridynn@users.noreply.github.com>
2026-02-11 19:15:56 +0100

    feat(ui): add favicon svg and configure app head with css and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
+	css: ["~/assets/css/main.css"],
+	app: {
+		head: {
+			title: "Eurowings Digital Frontend Challenge",
+			htmlAttrs: {
+				lang: "en",
+			},
+			link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+		},
+	},
 	typescript: {
 		typeCheck: true,
 	},
