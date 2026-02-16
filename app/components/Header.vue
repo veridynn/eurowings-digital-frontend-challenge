3e482456bdc4be3776cc23d0d01811b06d112c87
Bogdan <35661226+veridynn@users.noreply.github.com>
2026-02-16 14:53:25 +0100

    style[ui]: change header title from span to h1 for semantics

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,7 +2,7 @@
 	<UHeader :toggle="false" :ui="{ title: 'text-magenta' }">
 		<template #title>
 			<UIcon name="i-lucide-plane" class="size-7" />
-			<span>Fligthly</span>
+			<h1>Fligthly</h1>
 		</template>
 	</UHeader>
 </template>
